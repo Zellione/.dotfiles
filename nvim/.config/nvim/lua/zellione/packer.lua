621dfd6e4cdc03b8b2599cf341e74b665e592fa4
Zellione <nagels.sebastian@gmail.com>
2023-11-14 19:02:58 +0100

    Add vim-airline to nvim configuration

--- nvim/.config/nvim/lua/zellione/packer.lua
+++ nvim/.config/nvim/lua/zellione/packer.lua
@@ -4,72 +4,72 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.3',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  use {
-      'nvim-telescope/telescope-dap.nvim',
-      requires = {
-          {'nvim-telescope/telescope.nvim'},
-          {'mfussenegger/nvim-dap'}
-      }
-  }
+    use {
+        'nvim-telescope/telescope-dap.nvim',
+        requires = {
+            { 'nvim-telescope/telescope.nvim' },
+            { 'mfussenegger/nvim-dap' }
+        }
+    }
 
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  require("rose-pine").setup()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            require("rose-pine").setup()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
+    use('vim-airline/vim-airline')
 
-  use('terrortylor/nvim-comment')
-  use('tpope/vim-obsession')
+    use('terrortylor/nvim-comment')
+    use('tpope/vim-obsession')
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use {
-    'nvim-treesitter/nvim-treesitter-context',
-    requires = { { 'nvim-treesitter/nvim-treesitter' } }
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use {
+        'nvim-treesitter/nvim-treesitter-context',
+        requires = { { 'nvim-treesitter/nvim-treesitter' } }
 
-  }
-  use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-  use 'gerazov/toggle-bool.nvim'
+    }
+    use('nvim-treesitter/playground')
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use 'gerazov/toggle-bool.nvim'
 
-  use 'cdelledonne/vim-cmake'
-  use 'mfussenegger/nvim-dap'
-  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
-  use { 'ldelossa/nvim-dap-projects' }
-  use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v1.x',
-  requires = {
-	  -- LSP Support
-	  {'neovim/nvim-lspconfig'},             -- Required
-	  {'williamboman/mason.nvim'},           -- Optional
-	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use 'cdelledonne/vim-cmake'
+    use 'mfussenegger/nvim-dap'
+    use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
+    use { 'ldelossa/nvim-dap-projects' }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },  -- Required
+            { 'williamboman/mason.nvim' }, -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-	  -- Autocompletion
-	  {'hrsh7th/nvim-cmp'},         -- Required
-	  {'hrsh7th/cmp-nvim-lsp'},     -- Required
-	  {'hrsh7th/cmp-buffer'},       -- Optional
-	  {'hrsh7th/cmp-path'},         -- Optional
-	  {'saadparwaiz1/cmp_luasnip'}, -- Optional
-	  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'hrsh7th/cmp-buffer' }, -- Optional
+            { 'hrsh7th/cmp-path' }, -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
-	  -- Snippets
-	  {'L3MON4D3/LuaSnip'},             -- Required
-	  {'rafamadriz/friendly-snippets'}, -- Optional
-  }
-}
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },  -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Optional
+        }
+    }
 end)
-
