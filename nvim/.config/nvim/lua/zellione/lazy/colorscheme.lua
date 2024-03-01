149181d3c8458921473305efba13b4c7233b444f
Zellione <nagels.sebastian@gmail.com>
2024-03-01 16:06:38 +0100

    change neovim config

--- nvim/.config/nvim/lua/zellione/lazy/colorscheme.lua
+++ nvim/.config/nvim/lua/zellione/lazy/colorscheme.lua
@@ -1,9 +1,17 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
+	-- "folke/tokyonight.nvim",
+	-- lazy = false,
+	-- priority = 1000,
+	-- config = function()
+	-- 	vim.cmd.colorscheme("tokyonight-night")
+	--
+	-- 	vim.cmd.hi("Comment gui=none")
+	-- end,
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("tokyonight-storm")
+		vim.cmd.colorscheme("catppuccin-macchiato")
 
 		vim.cmd.hi("Comment gui=none")
 	end,
--- nvim/.config/nvim/lua/zellione/lazy/lsp.lua
+++ nvim/.config/nvim/lua/zellione/lazy/lsp.lua
@@ -107,6 +107,7 @@ return {
 
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
+			"codelldb",
 			"stylua",
 			"rust_analyzer",
 			"tsserver",
