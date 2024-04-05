eac895767fb7bd5cbd3adf29b875be0a1368d9f7
Zellione <nagels.sebastian@gmail.com>
2024-04-06 01:11:05 +0200

    Some adjustments

--- nvim/.config/nvim/lua/zellione/lazy/lsp.lua
+++ nvim/.config/nvim/lua/zellione/lazy/lsp.lua
@@ -100,7 +100,7 @@ return {
                             },
                         },
                         -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-                        -- diagnostics = { disable = { 'missing-fields' } },
+                        diagnostics = { disable = { 'missing-fields' } },
                     },
                 },
             },
