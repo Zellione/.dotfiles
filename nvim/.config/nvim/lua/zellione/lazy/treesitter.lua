dc865a5fc25fd16bdff38a35ed385f1e87c2bd2b
Zellione <nagels.sebastian@gmail.com>
2024-04-07 23:37:29 +0200

    Add incremental selection to treesitter config

--- nvim/.config/nvim/lua/zellione/lazy/treesitter.lua
+++ nvim/.config/nvim/lua/zellione/lazy/treesitter.lua
@@ -3,10 +3,36 @@ return {
     build = ':TSUpdate',
     config = function()
         require('nvim-treesitter.configs').setup {
-            ensure_installed = {'bash', 'c', 'cpp', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'rust', 'javascript', 'typescript', 'jsdoc' },
+            ensure_installed = {
+                'bash',
+                'c',
+                'cpp',
+                'html',
+                'lua',
+                'markdown',
+                'vim',
+                'vimdoc',
+                'rust',
+                'javascript',
+                'typescript',
+                'jsdoc'
+            },
             auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true }
+            highlight = {
+                enable = true
+            },
+            indent = {
+                enable = true
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = '<C-Space>',
+                    node_incremental = '<C-Space>',
+                    scope_incremental = false,
+                    node_decremental = '<bs>'
+                }
+            },
         }
     end
 }
