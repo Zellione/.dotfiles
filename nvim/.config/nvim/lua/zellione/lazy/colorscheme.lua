56bdf61160f80779d3602511087ad6f1ec0ce487
Zellione <nagels.sebastian@gmail.com>
2024-04-14 01:19:19 +0200

    Change theme to tokyo-night

--- nvim/.config/nvim/lua/zellione/lazy/colorscheme.lua
+++ nvim/.config/nvim/lua/zellione/lazy/colorscheme.lua
@@ -1,25 +1,25 @@
 return {
-    -- "folke/tokyonight.nvim",
-    -- lazy = false,
-    -- priority = 1000,
-    -- config = function()
-    -- 	vim.cmd.colorscheme("tokyonight-night")
-    --
-    -- 	vim.cmd.hi("Comment gui=none")
-    -- end,
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "folke/tokyonight.nvim",
+    lazy = false,
     priority = 1000,
     config = function()
-        require("catppuccin").setup({
-            integrations = {
-                mini = {
-                    enabled = true,
-                }
-            }
-        })
-        vim.cmd.colorscheme("catppuccin-mocha")
+    	vim.cmd.colorscheme("tokyonight-night")
 
-        vim.cmd.hi("Comment gui=none")
+    	vim.cmd.hi("Comment gui=none")
     end,
+    -- "catppuccin/nvim",
+    -- name = "catppuccin",
+    -- priority = 1000,
+    -- config = function()
+    --     require("catppuccin").setup({
+    --         integrations = {
+    --             mini = {
+    --                 enabled = true,
+    --             }
+    --         }
+    --     })
+    --     vim.cmd.colorscheme("catppuccin-mocha")
+    --
+    --     vim.cmd.hi("Comment gui=none")
+    -- end,
 }
