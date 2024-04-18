7b0e72b91e625f5009791feca659e43c1d32ceaf
Zellione <nagels.sebastian@gmail.com>
2024-04-19 00:11:29 +0200

    Update initial setup

--- hypr/.config/hypr/initial-boot.sh
+++ hypr/.config/hypr/initial-boot.sh
@@ -3,6 +3,7 @@
 # Variables
 scriptsDir="$HOME/.config/hypr/scripts"
 wallpaper="$HOME/wallpapers/CyberpunkLucy.jpg"
+kvantum_theme="Catppuccin-Mocha"
 
 swww="swww img"
 effect="--transition-bezier .43,1.19,1,.4 --transition-fps 30 --transition-type grow --transition-pos 0.925,0.977 --transition-duration 2"
@@ -19,14 +20,21 @@ if [ ! -f ~/.config/hypr/.initial_startup_done ]; then
     # Initial symlink for Pywal Dark and Light for Rofi Themes
     ln -sf "$HOME/.cache/wal/colors-rofi-dark.rasi" "$HOME/.config/rofi/pywal-color/pywal-theme.rasi" > /dev/null 2>&1 &
 
-    # initiate GTK dark mode and apply icon and cursor theme
+    # Initialize GTK dark mode and apply icon and cursor theme
     gsettings set org.gnome.desktop.interface color-scheme prefer-dark > /dev/null 2>&1 &
     gsettings set org.gnome.desktop.interface gtk-theme Tokyonight-Dark-BL-LB > /dev/null 2>&1 &
     gsettings set org.gnome.desktop.interface icon-theme Tokyonight-Dark > /dev/null 2>&1 &
     gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Ice > /dev/null 2>&1 &
     gsettings set org.gnome.desktop.interface cursor-size 24 > /dev/null 2>&1 &
 
-    "scriptsDir/switch_keyboard_layout.sh" > /dev/null 2>&1 &
+    # Initialize kvantum theme
+    kvantummanager --set "$kvantum_theme" > /dev/null 2>&1 &
+
+    # Initialize keyboard layout
+    "$scriptsDir/switch_keyboard_layout.sh" > /dev/null 2>&1 &
+
+    # Create a marker file to indiciate that the initial startup has been done
+    touch ~/.config/hypr/.initial_startup_done
 
     exit
 fi
