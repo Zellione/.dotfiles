19a875893bee049bf16cd9573d81c4b4657596b0
Sebastian Nagels <sebastian_nagels@deichmann.com>
2024-08-22 09:47:10 +0200

    configure conform

--- nvim/.config/nvim/lua/zellione/lazy/conform.lua
+++ nvim/.config/nvim/lua/zellione/lazy/conform.lua
@@ -2,10 +2,10 @@ return {
 	"stevearc/conform.nvim",
 	opts = {
 		notify_on_error = false,
-		-- format_on_save = {
-		-- 	timeout_ms = 500,
-		-- 	lsp_fallback = true,
-		-- },
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
 		formatters_by_ft = {
 			lua = { "stylua" },
 			-- Conform can also run multiple formatters sequentially
@@ -13,7 +13,30 @@ return {
 			--
 			-- You can use a sub-list to tell conform to run *until* a formatter
 			-- is found.
-			javascript = { { "prettierd", "prettier" } },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "prettierd", "prettier", stop_after_first = true },
+			html = { "prettierd", "prettier", stop_after_first = true },
+			scss = { "prettierd", "prettier", stop_after_first = true },
+			json = { "jq" },
+			go = { "goimports", "gofmt" },
+		},
+		-- formatters = {
+		-- 	shfmt = {
+		-- 		prepend_args = { "-i", "2" },
+		-- 	},
+		-- },
+	},
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ lsp_format = "fallback" })
+			end,
+			mode = "",
+			desc = "Format code",
 		},
 	},
+	config = function(_, opts)
+		require("conform").setup(opts)
+	end,
 }
