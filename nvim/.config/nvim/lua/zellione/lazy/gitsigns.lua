027a22d78afa426b1b9b4c8c0e656360402e6138
Sebastian Nagels <sebastian_nagels@deichmann.com>
2024-12-17 13:21:16 +0100

    remove git blame and use gitsigns blame instead

--- nvim/.config/nvim/lua/zellione/lazy/gitsigns.lua
+++ nvim/.config/nvim/lua/zellione/lazy/gitsigns.lua
@@ -7,5 +7,11 @@ return {
             delete = { text = '‾' },
             changedelete = { text = '~' }
         }
-    }
+    },
+	config = function(_, opts)
+		require("gitsigns").setup(opts)
+
+		vim.keymap.set("n", "<leader>gb", "<CMD>Gitsigns toggle_current_line_blame<cr>")
+		vim.keymap.set("n", "<leader>gbf", "<CMD>Gitsigns blame<cr>")
+	end,
 }
